6d3e074da49b8ba29cbfcc141315d3e77d141a9f
HerickCallegari <herickzeno@gmail.com>
2023-06-16 09:52:12 -0300

    web quase terminado

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+  <View className="flex-1 bg-red-700 justify-center items-center">
+    <Text className="text-3xl"></Text>
+  </View>
+  );
+}
+
